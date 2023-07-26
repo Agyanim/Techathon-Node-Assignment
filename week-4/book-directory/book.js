77c11d97d6e14ff6cdea6c347bc062d85c7b2427
agyanim <agyanimconsultancy@gmail.com>
2023-07-26 17:58:21 +0000

    first commit

--- week-4/book-directory/book.js
+++ week-4/book-directory/book.js
@@ -1,102 +1,135 @@
 const { readFile } = require("fs").promises;
-const { readFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 const { writeToFile } = require("./util");
 
-exports.createBook =async (req, res) => {
-    try {
-        let data = "";
-        req.on("data", (chunk) => {
-          data += chunk;
-        });
-        req.on("end", async () => {
-          data = JSON.parse(data);
-          if (!data.title || !data.author) {
-            console.log(data);
-            res.writeHead(400, { "Content-Type": "application/json" });
-            return res.end(JSON.stringify({ message: "All field must be provided" }));
-          }
-          const book = {
-            bookId: new Date().getMilliseconds(),
-            title: data.title,
-            author: data.author,
-            createdON: new Date().toLocaleString(),
-            modifiedOn: new Date().toLocaleString(),
-          };
-      
-        let fileData= await readFileSync("./book.json", "utf-8");
-            
-            fileData = JSON.parse(fileData);
-            fileData.push(book);
-           await writeToFile("./book.json", JSON.stringify(fileData), res);
-            res.writeHead(201, { "Content-Type": "application/json" });
-            return res.end(JSON.stringify({ message: "Success", Book: book }));
-          });
-           
-    } 
-    catch (error) {
-        
+exports.createBook = async (req, res) => {
+  try {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+    req.on("end", async () => {
+      data = JSON.parse(data);
+      if (!data.title || !data.author) {
+        console.log(data);
+        res.writeHead(400, { "Content-Type": "application/json" });
+        return res.end(
+          JSON.stringify({ message: "All field must be provided" })
+        );
+      }
+      const book = {
+        bookId: new Date().getMilliseconds(),
+        title: data.title,
+        author: data.author,
+        createdON: new Date().toLocaleString(),
+        modifiedOn: new Date().toLocaleString(),
+      };
+      console.log(typeof book.bookId);
+      let fileData = await readFileSync("./book.json", "utf-8");
+
+      fileData = JSON.parse(fileData);
+      fileData.push(book);
+      await writeFileSync("./book.json", JSON.stringify(fileData));
+      res.writeHead(201, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify({ message: "Success", Book: book }));
+    });
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ Error: error.message }));
+  }
+};
+exports.getAllBooks = async (res) => {
+  try {
+    let fileData = await readFileSync("./book.json", "utf-8");
+    fileData = JSON.parse(fileData);
+    res.writeHead(201, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ message: "Success", BookS: fileData }));
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ Error: error.message }));
+  }
+};
+
+exports.getBookById = async (res, id) => {
+  try {
+    let fileData = await readFileSync("./book.json", "utf-8");
+    fileData = JSON.parse(fileData);
+    const findBook = fileData.find((book) => book.bookId === +id);
+    if (!findBook) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify({ message: "No record found" }));
     }
+    console.log(findBook);
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ message: "Success", Book: findBook }));
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ Error: error.message }));
+  }
+};
+exports.deleteBook = async (res, id) => {
+  try {
+    let fileData = await readFileSync("./book.json", "utf-8");
+    fileData = JSON.parse(fileData);
+    // console.log(typeof(id));
+    const findBook = fileData.find((book) => book.bookId === +id);
+    if (!findBook) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "sorry, the record does not exit" })
+      );
+    }
+    console.log(findBook);
 
-}
-exports.getAllBooks=(res)=>{
-    readFile("./book.json", "utf-8", (err, fileData) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "application/json" });
-          return res.end(
-            JSON.stringify({ message: "Sorry, internal server error occurred" })
-          );
-        }
-        fileData = JSON.parse(fileData);
-        res.writeHead(201, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ message: "Success", BookS: fileData }));
-      });
-  
-}
+    const filteredBooks = fileData.filter((book) => book.bookId !== +id);
+    console.log(filteredBooks);
 
-exports.getBookById=(res,id)=>{
-    readFile("./book.json", "utf-8", (err, fileData) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "application/json" });
-          return res.end(
-            JSON.stringify({ message: "Sorry, internal server error occurred" })
-          );
-        }
-        fileData = JSON.parse(fileData);
-        const findBook=fileData.find(book=> book.bookId === +id)
-        if (!findBook){
-            res.writeHead(200, { "Content-Type": "application/json" });
-          return res.end(
-            JSON.stringify({ message: "No record found" })
-          );
-        }
+    await writeFileSync("./book.json", JSON.stringify(filteredBooks));
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(
+      JSON.stringify({ message: "Success", Books: filteredBooks })
+    );
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ Error: error.message }));
+  }
+};
+
+exports.updateBook = async (req, res, id) => {
+  try {
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    req.on("end", async () => {
+      data = JSON.parse(data);
+      const { title, author } = data;
+      let fileData = await readFileSync("./book.json", "utf-8");
+      fileData = JSON.parse(fileData);
+      // console.log(typeof(id));
+      const findBook = fileData.find((book) => book.bookId === +id);
+
+      if (!findBook) {
         res.writeHead(200, { "Content-Type": "application/json" });
-        return res.end(JSON.stringify({ message: "Success", Book: findBook }));
-      });
-  
-}
-exports.deleteBook=(res,id)=>{
-    readFile("./book.json", "utf-8", (err, fileData) => {
-        if (err) {
-          res.writeHead(500, { "Content-Type": "application/json" });
-          return res.end(
-            JSON.stringify({ message: "Sorry, internal server error occurred" })
-          );
-        }
-        fileData = JSON.parse(fileData);
-        const findBook=fileData.find(book=> book.bookId === +id)
-        if (!findBook){
-            res.writeHead(200, { "Content-Type": "application/json" });
-          return res.end(
-            JSON.stringify({ message: "sorry, the record does not exit" })
-          );
-        }
-        else{
+        return res.end(
+          JSON.stringify({ message: "sorry, the record does not exit" })
+        );
+      }
+      title !=undefined? findBook.title=title :findBook.title=findBook.title
+      author !=undefined? findBook.author=author :findBook.author=findBook.author
+console.log(findBook);
+      let filteredBooks = fileData.filter((book) => book.bookId !== +id);
 
-            const filteredBooks= fileData.filter(book=> book.bookId !==+ id)
-            writeToFile("./book.json",filteredBooks,res)
-            res.writeHead(200, { "Content-Type": "application/json" });
-            return res.end(JSON.stringify({ message: "Success", Books: filteredBooks }));
-        }
-      });
-  
-}
+      filteredBooks.push(findBook)
+    //   await writeFileSync("./book.json", JSON.stringify(filteredBooks));
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({ message: "Success", Book: findBook })
+      );
+    });
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ Error: error.message }));
+  }
+};
